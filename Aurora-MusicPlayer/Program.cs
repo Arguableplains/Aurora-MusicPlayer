5c32934e1b0fd7669fdc2f57541ee0aaf021ed20
Rhian Rodrigues Barbosa <rhianbarbosa2003@gmail.com>
2022-09-29 13:06:22 -0300

    Idea of Classes - testing

--- Aurora-MusicPlayer/Program.cs
+++ Aurora-MusicPlayer/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibVLCSharp.Shared;
+
+/*
+using var libvlc = new LibVLC(enableDebugLogs: true);
+using var media = new Media(libvlc, new Uri(""));
+using var mediaplayer = new MediaPlayer(media);
+
+mediaplayer.Play();
+*/
+
+
+BUGS J = new BUGS();
+
+J.Test();
+
+Console.ReadKey();
+
+#region
+
+public class UserActions
+    {
+        // Choosing Menu - A class that migth always run, just waiting for instructions...
+    }
+
+public class LoadingProcess
+{
+    //BackEnd - Where things will be loaded and prepared for what is coming
+}
+
+public class BUGS
+    {
+        public void Test()
+        {
+            Console.WriteLine("EGG");
+        }
+    }
+
+
+#endregion
+
